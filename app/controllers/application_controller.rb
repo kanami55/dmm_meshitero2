0eafa6ec297e74ddacd53d2fd34e38470bef921a
Higashimoto Kanami <summerwave5959@gmail.com>
2022-01-23 01:16:04 +0000

    [Add] devise追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+ #サインイン後にどこに遷移するのか（devise側で用意されているメソッド)
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+
+ #ログアウト後にどこに遷移するのか（devise側で用意されているメソッド)
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
