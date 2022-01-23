03e66fa71acee90dab46fcac7c43b33105d802b6
Higashimoto Kanami <summerwave5959@gmail.com>
2022-01-23 09:21:16 +0000

    [Add] いいね機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'homes/about' => 'homes#about', as: 'about'
-  resources :post_images, onry: [:new,:create, :index, :show, :destroy]
-  resources :users, onry: [:show, :edit, :update]
+  resources :post_images, onry: [:new,:create, :index, :show, :destroy] do
+  resource :favorites, only: [:create, :destroy]
   resources :post_comments, only: [:create,:destroy]
+end
+  resources :users, onry: [:show, :edit, :update]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
